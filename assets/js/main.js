5d6298089fcabe568c2ba9aded6bb7be211b366b
Gileandro Campos <102332042+gilecampos@users.noreply.github.com>
2023-02-11 17:47:42 -0300

    :lipstick: feat: Add accordion in the page
    Add accordion and add styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-const btnMobile = document.querySelector("[data-js=btnMobile]");
 
+// BTN MENU MOBILE
+const btnMobile = document.querySelector("[data-js=btnMobile]");
 
 function toggleMenu(event) {
    if(event.type === 'touchstart') event.preventDefault();
@@ -19,3 +20,21 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+// ACCORDION 
+const acc = document.querySelectorAll(".accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
